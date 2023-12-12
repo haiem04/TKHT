59a42fbea26e89ae45ad52d3b29a2c9373d351ea
ngsontung <ngsondecon@gmail.com>
2023-12-12 17:12:46 +0700

    Update tool strip menu

--- TKHT/PView/FTestSelect.Designer.cs
+++ TKHT/PView/FTestSelect.Designer.cs
@@ -45,22 +45,21 @@
             this.toolStripMenuItem1.Margin = new System.Windows.Forms.Padding(25, 0, 0, 0);
             this.toolStripMenuItem1.Name = "toolStripMenuItem1";
             this.toolStripMenuItem1.Padding = new System.Windows.Forms.Padding(40, 0, 4, 0);
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(120, 29);
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(96, 20);
             this.toolStripMenuItem1.Text = "Logout";
             this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
             // 
             // menuStrip1
             // 
             this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(28)))), ((int)(((byte)(104)))));
-            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripMenuItem2,
             this.toolStripMenuItem1});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(6, 15, 0, 1);
-            this.menuStrip1.Size = new System.Drawing.Size(1196, 45);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 10, 0, 1);
+            this.menuStrip1.Size = new System.Drawing.Size(797, 31);
             this.menuStrip1.TabIndex = 117;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -71,8 +70,8 @@
             this.toolStripMenuItem2.Margin = new System.Windows.Forms.Padding(25, 0, 0, 0);
             this.toolStripMenuItem2.Name = "toolStripMenuItem2";
             this.toolStripMenuItem2.Padding = new System.Windows.Forms.Padding(40, 0, 4, 0);
-            this.toolStripMenuItem2.Size = new System.Drawing.Size(116, 29);
-            this.toolStripMenuItem2.Text = "Admin";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(92, 20);
+            this.toolStripMenuItem2.Text = "Home";
             this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
             // 
             // dgvTests
@@ -81,11 +80,10 @@
             this.dgvTests.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(59)))), ((int)(((byte)(122)))));
             this.dgvTests.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.dgvTests.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvTests.Location = new System.Drawing.Point(32, 169);
-            this.dgvTests.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dgvTests.Location = new System.Drawing.Point(21, 110);
             this.dgvTests.Name = "dgvTests";
             this.dgvTests.RowHeadersWidth = 51;
-            this.dgvTests.Size = new System.Drawing.Size(1130, 648);
+            this.dgvTests.Size = new System.Drawing.Size(753, 421);
             this.dgvTests.TabIndex = 124;
             this.dgvTests.CellContentDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvTests_CellContentDoubleClick);
             // 
@@ -94,10 +92,10 @@
             this.comboBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(59)))), ((int)(((byte)(122)))));
             this.comboBox1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(147, 101);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.comboBox1.Location = new System.Drawing.Point(98, 66);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(203, 28);
+            this.comboBox1.Size = new System.Drawing.Size(137, 21);
             this.comboBox1.TabIndex = 125;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
@@ -107,25 +105,24 @@
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.textBox1.Location = new System.Drawing.Point(32, 102);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Location = new System.Drawing.Point(21, 66);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(108, 28);
+            this.textBox1.Size = new System.Drawing.Size(72, 19);
             this.textBox1.TabIndex = 126;
             this.textBox1.Text = "Subject: ";
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // FTestSelect
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(28)))), ((int)(((byte)(104)))));
-            this.ClientSize = new System.Drawing.Size(1196, 852);
+            this.ClientSize = new System.Drawing.Size(797, 554);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.dgvTests);
             this.Controls.Add(this.menuStrip1);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FTestSelect";
             this.Text = "Form1";
             this.menuStrip1.ResumeLayout(false);
